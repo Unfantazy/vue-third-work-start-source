752aa4a33ec6b0b9d21d2810e5977ef93f94b6a1
unfantazy <akuptsova@korusconsulting.ru>
2025-01-20 12:22:09 +0300

    section 1

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>
+    <button @click="decrement">-</button>
+
+    <span> Current value: {{ count }} </span>
+
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
